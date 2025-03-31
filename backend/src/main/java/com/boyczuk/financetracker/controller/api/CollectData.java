a5a5a7f12eac3b0a3677bae9d25ce8e43590a52a
adlai <adlaibridsonboyczuk@gmail.com>
2025-03-31 01:26:29 -0400

    Added API endpoint for getting transactions in range

--- backend/src/main/java/com/boyczuk/financetracker/controller/api/CollectData.java
+++ backend/src/main/java/com/boyczuk/financetracker/controller/api/CollectData.java
@@ -1,23 +1,31 @@
 package com.boyczuk.financetracker.controller.api;
 
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boyczuk.financetracker.service.CSVImportService;
+import com.boyczuk.financetracker.model.Transaction;
+import com.boyczuk.financetracker.repository.TransactionRepository;
 
 @RestController
 public class CollectData {
 
     private final CSVImportService csvImportService;
+    private final TransactionRepository transactionRepository;
 
-    public CollectData(CSVImportService csvImportService) {
+    public CollectData(CSVImportService csvImportService, TransactionRepository transactionRepository) {
         this.csvImportService = csvImportService;
+        this.transactionRepository = transactionRepository;
     }
-    
+
     @GetMapping("/api/collect")
     public String getData() {
         return "Gathering csv file";
@@ -33,5 +41,12 @@ public class CollectData {
         InputStream inputStream = csvImportService.importCSV();
         csvImportService.saveToDB(inputStream);
         return ResponseEntity.ok("Import triggered");
+    }
+
+    @GetMapping("/api/transactions")
+    public List<Transaction> getTransactionsInRange(
+            @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+            @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
+                return transactionRepository.findByDateBetween(start, end);
     }
 }
--- backend/src/main/java/com/boyczuk/financetracker/repository/TransactionRepository.java
+++ backend/src/main/java/com/boyczuk/financetracker/repository/TransactionRepository.java
@@ -1,7 +1,13 @@
 package com.boyczuk.financetracker.repository;
 
 import com.boyczuk.financetracker.model.Transaction;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByDateBetween(LocalDate start, LocalDate end);
+    
 }
